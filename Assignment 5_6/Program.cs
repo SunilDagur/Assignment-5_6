25315680e9b2bb50c6c8769915a39165bd93c43e
Sunil Dagur <sunildagur566@gmail.com>
2023-12-06 17:59:38 +0530

    Add project files.

---
+++ Assignment 5_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Outer try block - Start");
+
+            
+            try
+            {
+                Console.WriteLine("Inner try block - Start");
+
+               
+                throw new Exception("Exception in the inner try block");
+
+                Console.WriteLine("Inner try block - End"); 
+            }
+            catch (Exception innerException)
+            {
+                
+                Console.WriteLine($"Caught exception in the inner catch block: {innerException.Message}");
+            }
+
+            Console.WriteLine("Outer try block - End"); 
+        }
+        catch (Exception outerException)
+        {
+           
+            Console.WriteLine($"Caught exception in the outer catch block: {outerException.Message}");
+        }
+    }
+}
